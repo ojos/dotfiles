e9ef08226bfeaa532923f0f11ac6adbb9262f4b3
Ido <ido@ojos.jp>
2019-11-05 22:27:21 +0900

    added fish_prompt.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,20 @@
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
 # bindings
 fish_vi_key_bindings
 
@@ -32,5 +49,5 @@ end
 set -x GHQ_SELECTOR peco
 
 # dircolrs
-eval (dircolors ~/.dircolors/dircolors | head -n 1 | sed 's/^LS_COLORS=/set -x LS_COLORS /;s/;$//')
+eval (dircolors ~/.dir_colors/dircolors | head -n 1 | sed 's/^LS_COLORS=/set -x LS_COLORS /;s/;$//')
 
